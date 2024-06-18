46d801569ba90f876850bd811bf651c6e44baefa
Jason Barden <jason.barden@outlook.com>
2024-06-18 20:35:40 +0100

    Remove unnecessary code now we're using the NuGet packages again

--- tests/unit/ui/AStar.Web.UI.Unit.Tests/MockMessageHandlers/MockHttpRequestExceptionErrorHttpMessageHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AStar.Web.UI.MockMessageHandlers;
-
-public class MockHttpRequestExceptionErrorHttpMessageHandler : HttpMessageHandler
-{
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        => throw new HttpRequestException();
-}
